748641c97ed2392ee40bbc0e7c7325fa6728870c
Gino Luiggi Chirinos Plasencia <gchirinos@factorenergia.com>
2020-03-12 10:33:25 +0100

    Cambios sobre lazy loading

--- src/app/pages/default-image/default-image.component.ts
+++ src/app/pages/default-image/default-image.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./default-image.component.scss']
 })
 export class DefaultImageComponent {
+  /*
   errorImage: string = 'https://i.imgur.com/XkU4Ajf.png';
   defaultImage: string = 'https://www.placecage.com/1000/1000';
 
@@ -14,4 +15,14 @@ export class DefaultImageComponent {
     'https://images.unsplash.com/photo-1468103933896-2c34a78104c2?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80',
     'https://images.unsplash.com/photo-1471201187657-6406da15e43b?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80'
   ];
+  */
+
+ errorImage: string = 'assets/01.jpg';
+ defaultImage: string = 'assets/01.jpg';
+
+ images: any[] = [
+  'assets/01.jpg',
+  'assets/02.jpg',
+  'assets/03.jpg',
+];
 }
--- src/app/pages/fade-in-image/fade-in-image.component.ts
+++ src/app/pages/fade-in-image/fade-in-image.component.ts
@@ -7,12 +7,25 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FadeInImageComponent {
+  /*
   errorImage: string = 'https://i.imgur.com/XkU4Ajf.png';
   defaultImage: string = 'https://www.placecage.com/1000/1000';
+  */
 
+  /*
   images: any[] = [
     'https://images.unsplash.com/photo-1468413922365-e3766a17da9e?dpr=2&auto=compress,format&fit=crop&w=1199&h=800&q=80',
     'https://images.unsplash.com/photo-1488388373205-a134c1cc7e4e?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80',
     'https://images.unsplash.com/photo-1422257986712-4f02edc298ce?dpr=2&auto=compress,format&fit=crop&w=1199&h=1199&q=80'
   ];
+  */
+
+ errorImage: string = 'assets/01.jpg';
+ defaultImage: string = 'assets/01.jpg';
+
+ images: any[] = [
+  'assets/01.jpg',
+  'assets/02.jpg',
+  'assets/03.jpg',
+];
 }
